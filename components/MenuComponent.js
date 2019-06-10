bcd7da44b7d30633f08374a4eb59cc5e79e47ef8
cjchirag7 <cjchirag7@gmail.com>
2019-06-10 17:16:35 +0530

    React Navigation 1

--- components/MenuComponent.js
+++ components/MenuComponent.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React,{Component} from 'react';
 import { View, FlatList } from 'react-native';
 import { ListItem } from 'react-native-elements';
+import { DISHES } from '../shared/dishes';
 
-function Menu(props) {
+class Menu extends Component{
+constructor(props) {
+    super(props);
+    this.state={
+        dishes: DISHES
+    };
+}
 
+    static navigationOptions={
+                              title: 'Menu'
+                              };
+
+    render(){
     const renderMenuItem = ({item, index}) => {
-
         return (
                 <ListItem
                     key={index}
@@ -13,18 +24,20 @@ function Menu(props) {
                     subtitle={item.description}
                     hideChevron={true}
                     leftAvatar={{ source: require('./images/uthappizza.png')}}
-                    onPress={()=>props.onPress(item.id)}
+                    onPress={()=>navigate('DishDetail',{dishId: item.id})}
                   />
         );
     };
 
+    const { navigate } =this.props.navigation;
     return (
             <FlatList 
-                data={props.dishes}
+                data={this.state.dishes}
                 renderItem={renderMenuItem}
                 keyExtractor={item => item.id.toString()}
                 />
     );
+    }
 }
 
 
